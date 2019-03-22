52a8a94c2a55c90e684199a4d10a4f912aa1f7c3
jingqiaoxu <jingqiaoxu123@gmail.com>
2019-03-21 22:17:04 -0500

    add file

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -9,6 +9,8 @@ public interface DBConnection {
 	/**
 	 * Close the connection.
 	 */
+	public boolean registerUser(String userId, String password, String firstname, String lastname);
+
 	public void close();
 
 	/**
--- src/db/mysql/MySQLConnection.java
+++ src/db/mysql/MySQLConnection.java
@@ -49,7 +49,7 @@ public class MySQLConnection implements DBConnection {
 	   	       }
 	   	
 	   	      try {
-	   		 String sql = "INSERT IGNORE INTO history(user_id, item_id) VALUES (?, ?)";
+	   		 String sql = "INSERT INTO history (user_id, item_id) VALUES (?, ?)";
 	   		 PreparedStatement ps = conn.prepareStatement(sql);
 	   		 ps.setString(1, userId);
 	   		 for (String itemId : itemIds) {
@@ -239,13 +239,75 @@ public class MySQLConnection implements DBConnection {
 	@Override
 	public String getFullname(String userId) {
 		// TODO Auto-generated method stub
-		return null;
+		if (conn == null) {
+			return "";
+		}
+		
+		String name = "";
+		try {
+			String sql = "SELECT first_name, last_name FROM users WHERE user_id = ? ";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, userId);
+			ResultSet rs = statement.executeQuery();
+			
+			while (rs.next()) {
+				name = rs.getString("first_name") + " " + rs.getString("last_name");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return name;
+
 	}
 
 	@Override
 	public boolean verifyLogin(String userId, String password) {
 		// TODO Auto-generated method stub
-		return false;
-	}
+			if (conn == null) {
+				return false;
+			}
+		
+			try {
+				String sql="SELECT * FROM users WHERE user_id = ? AND password = ? ";
+				PreparedStatement statement=conn.prepareStatement(sql);
+				statement.setString(1, userId);
+				statement.setString(2, password);
+				ResultSet rs=statement.executeQuery();
+				while (rs.next()) {
+					return true;
+				}
+			} catch (SQLException e){
+				e.printStackTrace();
+			}
 
+		
+			return false;
+		}
+
+
+	@Override
+	public boolean registerUser(String userId, String password, String firstname, String lastname) {
+		// TODO Auto-generated method stub
+		if (conn == null) {
+			System.err.println("DB connection failed");
+			return false;
+		}
+
+		try {
+			String sql = "INSERT IGNORE INTO users VALUES (?, ?, ?, ?)";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, userId);
+			ps.setString(2, password);
+			ps.setString(3, firstname);
+			ps.setString(4, lastname);
+			
+			return ps.executeUpdate() == 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;	
+	}
 }
+
+
